23599c437f1f7840dac7f1bfbf8aa65f0be61056
Ruslan Birzhanov <birganovruslan@yandex.ru>
2020-08-07 14:09:14 +0700

    Added example table & persons' display function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./App.css";
 
 class App extends Component {
+  data = [
+    { id: "1", firstName: "Test1", lastName: "Testov1" },
+    { id: "2", firstName: "Test2", lastName: "Testov2" },
+    { id: "3", firstName: "Test3", lastName: "Testov3" },
+  ];
+
+  DisplayUsers = (user, id) => {
+    return (
+      <tr key={id}>
+        <td>{user.id}</td>
+        <td>{user.firstName}</td>
+        <td>{user.lastName}</td>
+      </tr>
+    );
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <Container fluid>
+            <Row>
+              <Col></Col>
+              <Col md={8}>
+                <Table responsive striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Surname</th>
+                    </tr>
+                  </thead>
+                  <tbody>{this.data.map(this.DisplayUsers)}</tbody>
+                </Table>
+              </Col>
+              <Col></Col>
+            </Row>
+          </Container>
+        </header>
       </div>
     );
   }
