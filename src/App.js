13d83bd24d6cf0570e25901a1af6c3f2d193e1b1
Ruslan Birzhanov <birganovruslan@yandex.ru>
2020-08-07 15:25:57 +0700

    Added Navbar, Icons and Buttons

--- src/App.js
+++ src/App.js
@@ -1,51 +1,85 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Header from "./Components/Header";
+
 import Table from "react-bootstrap/Table";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserEdit,
+  faTimes,
+  faUserTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./App.css";
 
 class App extends Component {
-  data = [
-    { id: "1", firstName: "Test1", lastName: "Testov1" },
-    { id: "2", firstName: "Test2", lastName: "Testov2" },
-    { id: "3", firstName: "Test3", lastName: "Testov3" },
-  ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      isDataLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:4000/persons")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          isDataLoaded: true,
+          data: json,
+        });
+      });
+  }
 
   DisplayUsers = (user, id) => {
     return (
       <tr key={id}>
-        <td>{user.id}</td>
         <td>{user.firstName}</td>
         <td>{user.lastName}</td>
+        <td>
+          <div className="table-buttons">
+            <Button variant="outline-primary">
+              <FontAwesomeIcon icon={faUserEdit} />
+            </Button>{" "}
+            <Button variant="outline-danger">
+              <FontAwesomeIcon icon={faUserTimes} />
+            </Button>
+          </div>
+        </td>
       </tr>
     );
   };
 
   render() {
+    const data = this.state.data;
     return (
       <div className="App">
         <header className="App-header">
-          <Container fluid>
-            <Row>
-              <Col></Col>
-              <Col md={8}>
-                <Table responsive striped bordered hover>
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Name</th>
-                      <th>Surname</th>
-                    </tr>
-                  </thead>
-                  <tbody>{this.data.map(this.DisplayUsers)}</tbody>
-                </Table>
-              </Col>
-              <Col></Col>
-            </Row>
-          </Container>
+          <Header />
         </header>
+        <Container fluid>
+          <Row>
+            <Col></Col>
+            <Col md={8}>
+              <Table responsive striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Имя</th>
+                    <th>Фамилия</th>
+                  </tr>
+                </thead>
+                <tbody>{data.map(this.DisplayUsers)}</tbody>
+              </Table>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
       </div>
     );
   }
